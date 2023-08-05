cf989773e6837bf499c5d3de35e52ff3919027b8
Abelardo Palácios <71283403+AbelardoOk@users.noreply.github.com>
2023-08-05 00:00:15 -0400

    feat: add back card

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,22 +1,25 @@
 import Image from "next/image";
 
-interface Dados {
+export function Card({
+  cardNumber,
+  titName,
+  validate,
+  cvv,
+}: {
   cardNumber: string;
   titName: string;
   validate: string;
   cvv: string;
-}
-
-export function Card(props: Dados) {
+}) {
   return (
-    <div className="flex flex-col p-6 bg-gradient-to-r from-[#E68E7B] to-[#D47A66] gap-8 w-max">
+    <div className="flex flex-col w-[500px] h-[280px] p-6 bg-gradient-to-r from-[#E68E7B] to-[#D47A66] gap-8">
       <Image src="/logo.png" alt="" width={192} height={37.65} />
 
       <div className="text-white text-left flex flex-col">
-        <p className="font-bold text-2xl">1234 1234 1234 1234</p>
-        <p className="font-medium text-base">Leonardo di Ser Piero da Vinci</p>
+        <p className="font-bold text-2xl">{cardNumber}</p>
+        <p className="font-medium text-base">{titName}</p>
         <p className="font-normal text-xs">Valido até</p>
-        <p className="font-bold text-xs">01/42</p>
+        <p className="font-bold text-xs">{validate}</p>
       </div>
     </div>
   );
---
+++ src/components/cardBack.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export function CardBack(cvv: string) {
+  return (
+    <div className="flex flex-col w-[500px] h-[280px] p-6 bg-gradient-to-r from-[#E68E7B] to-[#D47A66] gap-8">
+      <div className="px-6 pt-6 bg-[#A45A49]"></div>
+      <div className="items-end flex flex-col">
+        <div className="text-white text-end flex flex-col">
+          <p className="font-normal text-base">CVV</p>
+          <p className="font-bold text-base">{cvv}</p>
+        </div>
+        <Image src="/iconDiscord.png" alt="" width={36} height={36} />
+      </div>
+    </div>
+  );
+}
